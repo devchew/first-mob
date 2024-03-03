09603f1c15b68b3082c227f3d01d4b3a1a5ac2c8
DevChew <the@devchew.com>
2024-03-03 11:53:57 +0100

    counter

--- app/src/main/java/com/example/first_mob/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/first_mob/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.example.first_mob.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -16,6 +17,8 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private var counter = 1;
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,8 +35,15 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        binding.button.setOnClickListener {
+            counter++;
+            textView.text = counter.toString();
+        }
+
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
